5184dbc10d0c6b879204876d17e9e965afc8eae7
ZakariaTaoussi <zakaria.taoussi03@gmail.com>
2026-05-01 14:40:55 +0200

    implementation eslint & prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import express from 'express';
-const app = express() ; 
-
-const PORT = process.env.PORT || 3000 ;
+const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World !') ;
-}) ; 
-export default app ;
+  res.status(200).send('Hello World !');
+});
+export default app;
