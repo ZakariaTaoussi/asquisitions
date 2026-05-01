ad5bb6c82e16f3d57ccfed53bf22aad4a809e2e2
ZakariaTaoussi <zakaria.taoussi03@gmail.com>
2026-05-01 20:09:59 +0200

    set up neon postgre and drizzle

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+
+export default { 
+    schema : './src/models/*.js',
+    out : './drizzle',
+     driver: "pg",
+    dbCredentials : {
+         connectionString: process.env.DATABASE_URL,
+    }
+}
---
+++ src/models/user.model.js
@@ -0,0 +1,17 @@
+import { pgTable, serial, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+
+  name: varchar("name", { length: 255 }).notNull(),
+
+  email: varchar("email", { length: 255 }).notNull().unique(),
+
+  passwordHash: varchar("password_hash", { length: 255 }).notNull(),
+
+  role: varchar("role", { length: 50 }).notNull().default("user"),
+
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
