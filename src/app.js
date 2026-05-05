597226894456fa488265a438d53adc44f426366e
ZakariaTaoussi <zakaria.taoussi03@gmail.com>
2026-05-05 16:33:02 +0200

    add Loggers & cookies & jwt

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 import express from 'express';
+import logger from './config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.routes.js';
 const app = express();
-
+app.use(cors());
+app.use(cookieParser());    
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
+app.use(helmet());
 app.get('/', (req, res) => {
+  logger.info('Hello World from logger!');
   res.status(200).send('Hello World !');
 });
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'API is working' });
+});
+
+app.use('/api/auth', authRoutes);
 export default app;
---
+++ src/utils/jwt.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import logger from '../config/logger';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
+
+const JWT_EXPIRATION = '1d'; // Token expires in 1 day
+
+ export const jwttoken = { 
+    sign:(payload) => { 
+        try { 
+            return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRATION });
+        }catch (e){ 
+            logger.error('Error signing JWT:', e);
+            throw new Error('Failed to sign JWT');
+        }
+    }
+    verify: (token) => { 
+        try { 
+            return jwt.verify(token, JWT_SECRET);
+        }catch (e){ 
+            logger.error('Error verifying JWT:', e);
+            throw new Error('Failed to verify JWT');
+        }
+    }
+ }
